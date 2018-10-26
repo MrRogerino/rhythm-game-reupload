c6d3f71dd4d41621d8fe071eae5595e1e3a6fcf1
Roger <lakersli@gmail.com>
2018-10-26 14:35:02 -0700

    Init commit

---
+++ Assets/Activator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour {
+
+    public KeyCode key;
+    bool active = false;
+    GameObject note;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(key) && active)
+        {
+            Debug.Log("note destroyed");
+            Destroy(note);
+            active = false;
+        }
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        active = true;
+        Debug.Log("note active:" + active);
+        if (col.gameObject.CompareTag("Note"))
+        {
+            note = col.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        active = false;
+        Debug.Log("note active:" + active);
+    }
+}
---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public int movespeed = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.forward * movespeed * Time.deltaTime;
+    }
+}
