a0025afa6a30d50f74247c57e5f7d553ac6b6d32
Roger <weihsuan.li24@gmail.com>
2018-11-30 01:22:18 -0700

    create skybox change script

---
+++ Assets/Colliders/Skybox/SkyboxChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxChange : MonoBehaviour {
+
+    public Material skyboxMaterial;
+
+    void OnTriggerEnter (Collider col)
+    {
+        if (col.gameObject.CompareTag("NoteBar")){
+            RenderSettings.skybox = skyboxMaterial;
+        }
+    }
+}
