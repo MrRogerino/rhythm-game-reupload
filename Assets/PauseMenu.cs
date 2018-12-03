51a7542ff03600fc3fe44556bce2ffa4b8741f58
Roger <weihsuan.li24@gmail.com>
2018-12-02 22:09:57 -0700

    Implement pause and resume function

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    AudioSource playerAudio;
+    AudioSource baseAudio;
+
+    void Start()
+    {
+        playerAudio = GameObject.Find("PlayerTrack").GetComponent<AudioSource>();
+        baseAudio = GameObject.Find("BaseTrack").GetComponent<AudioSource>();
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        playerAudio.Pause();
+        baseAudio.Pause();
+        Debug.Log("paused!");
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        playerAudio.Play();
+        baseAudio.Play();
+        Debug.Log("resume!");
+    }
+
+}
